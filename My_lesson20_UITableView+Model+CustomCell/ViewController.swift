c14ff942d204c77b605cfe3adda12be080937569
chydikO <oleg.chudnovskiy@gmail.com>
2020-05-31 12:57:42 +0300

    empty project

---
+++ My_lesson20_UITableView+Model+CustomCell/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  My_lesson20_UITableView+Model+CustomCell
+//
+//  Created by Олег Чудновский on 31.05.2020.
+//  Copyright © 2020 Олег Чудновский. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
